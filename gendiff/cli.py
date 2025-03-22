8789ab6e7bf40f5ecdd2ad8c699c7e56fa4815c5
Oxana Shu <laskka9@gmail.com>
2025-03-22 10:37:31 +0300

    after step 1

---
+++ gendiff/cli.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def pars_args():
+    parser = argparse.ArgumentParser(
+        prog="gendiff",
+        description="Compares two configuration files and shows a difference.",
+    )
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    return parser.parse_args()
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from gendiff.cli import pars_args
+
+
+def main():
+    pars_args()
+
+
+if __name__ == '__main__':
+    main()
